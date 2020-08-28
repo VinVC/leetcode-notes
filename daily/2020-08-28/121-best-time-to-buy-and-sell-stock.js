ce071407c97dcfaaf2d41376ab085c3a6b308445
Vincent.liu <1241935691@qq.com>
2020-08-28 23:56:23 +0800

    :hammer:rename all of cn file names and change the format of files's name

---
+++ daily/2020-08-28/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+
+注意：你不能在买入股票前卖出股票。
+
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+
+思路：
+1.遍历数组，当为i时,和全局的最小值比较
+2.i与当前最小值的差值，和最大的利润值进行比较
+ */
+var maxProfit = function (prices) {
+  let minPrice = Number.MAX_VALUE, maxProfit = 0
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) minPrice = prices[i]
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice)
+  }
+  return maxProfit
+};
