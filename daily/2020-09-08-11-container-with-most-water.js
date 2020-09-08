79d6d525fc9a7f8184c75dff1accd5cdc065d0c4
Vincent.liu <1241935691@qq.com>
2020-09-08 22:59:09 +0800

    :new:add 11

---
+++ daily/2020-09-08-11-container-with-most-water.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let l = 0, r = height.length - 1, ans = 0
+  while (l < r) {
+    let area = Math.min(height[l], height[r]) * (r - l)
+    ans = Math.max(area, ans)
+    if (height[l] <= height[r]) {
+      l++
+    } else {
+      r--
+    }
+  }
+  return ans
+};
