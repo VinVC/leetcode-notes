b8d8550d9a8f23bda588618c2f7eda2ecbe5a091
Vincent.liu <1241935691@qq.com>
2020-09-26 23:33:47 +0800

    :new: add new dp

--- problems/dp/数组中是否存在和为target的子序列.js
+++ problems/dp/数组中是否存在和为target的子序列.js
@@ -8,11 +8,32 @@ function recursionSubset(arr, i, s) {
 
 }
 
-function dpSubset(arr, s) {
+function dpSubset(arr, target) {
   let subset = new Array(arr.length)
   for (let i = 0; i < subset.length; i++) {
-    subset[i] = new Array(s + 1)
+    subset[i] = new Array(target + 1)
   }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < target + 1; j++) {
+      if (j === 0) {
+        subset[i][j] = true
+        continue;
+      }
+      if (i === 0) {
+        subset[i][j] = arr[0] === j
+        continue;
+      }
+      if (arr[i] > j) {
+        subset[i][j] = subset[i - 1][j]
+      } else {
+        let a = subset[i - 1][j - arr[i]]
+        let b = subset[i - 1][j]
+        subset[i][j] = a || b
+      }
+    }
+  }
+  return subset[arr.length - 1][target]
+  // return subset
 }
 
 
@@ -24,4 +45,20 @@ function dpSubset(arr, s) {
 const arr = [3, 34, 4, 12, 5, 2]
 
 console.log(recursionSubset(arr, arr.length - 1, 9))
-console.log(recursionSubset(arr, arr.length - 1, 0))
+console.log(recursionSubset(arr, arr.length - 1, 10))
+console.log(recursionSubset(arr, arr.length - 1, 11))
+console.log(recursionSubset(arr, arr.length - 1, 12))
+console.log(recursionSubset(arr, arr.length - 1, 13))
+console.log(recursionSubset(arr, arr.length - 1, 14))
+console.log(recursionSubset(arr, arr.length - 1, 15))
+
+// dp
+console.log('------分割线--------')
+console.log(dpSubset(arr, 9))
+console.log(dpSubset(arr, 10))
+console.log(dpSubset(arr, 11))
+console.log(dpSubset(arr, 12))
+console.log(dpSubset(arr, 13))
+console.log(dpSubset(arr, 14))
+console.log(dpSubset(arr, 15))
+
