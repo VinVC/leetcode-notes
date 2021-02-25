65e225db2e8db3f924b04000f79101cf3ce9cfe9
hongchaoliu <hongchaoliu@6tiantian.com>
2021-02-25 15:53:52 +0800

    :new: ADD twosum2

---
+++ daily/2021-02-25/twoSum.js
@@ -0,0 +1,25 @@
+/**
+ * 给定一个已按照 升序排列  的整数数组 numbers ，请你从数组中找出两个数满足相加之和等于目标数 target 。
+
+函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 1 开始计数 ，所以答案数组应当满足 1 <= answer[0] < answer[1] <= numbers.length 。
+
+你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let l = 0,
+    r = numbers.length - 1;
+  while (l < r) {
+    let sum = numbers[l] + numbers[r];
+    if (sum === target) {
+      return [l + 1, r + 1];
+    } else if (sum < target) {
+      l++;
+    } else {
+      r--;
+    }
+  }
+  return [];
+};
