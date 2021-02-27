c7acf52d28342f1441a3f815f8516d0c28fd3934
Vincent.liu <1241935691@qq.com>
2021-02-27 10:33:58 +0800

    :pencil2: Complete SearchInRotatedSortedArray

--- daily/2021-02-26/searchInRotatedSortedArray.js
+++ daily/2021-02-26/searchInRotatedSortedArray.js
@@ -25,18 +25,29 @@
  * @return {number}
  */
 var search = function (nums, target) {
-  let len = nums.length;
-  if (!len) return -1;
+  let len = nums.length
+  if (!len) return -1
   if (len === 1) {
-    return nums[0] === target ? 0 : -1;
+    return nums[0] === target ? 0 : -1
   }
   let left = 0,
-    right = len - 1;
+    right = len - 1
   while (left <= right) {
-    let mid = ((right - left) >> 1) + left;
-    if (nums[mid] === target) return mid;
+    let mid = ((right - left) >> 1) + left
+    if (nums[mid] === target) return mid
     if (nums[0] <= nums[mid]) {
+      if (nums[0] <= target && target < nums[mid]) {
+        right = mid - 1
+      } else {
+        left = mid + 1
+      }
     } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
     }
   }
-};
+  return -1
+}
