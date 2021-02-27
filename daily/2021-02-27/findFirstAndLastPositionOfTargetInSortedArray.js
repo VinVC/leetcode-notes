5f3e0d21831083367d3940b46b1f4af49b4bcae0
Vincent.liu <1241935691@qq.com>
2021-02-27 11:28:19 +0800

    :new: ADD findFirstAndLastPositionOfTargetInSortedArray

---
+++ daily/2021-02-27/findFirstAndLastPositionOfTargetInSortedArray.js
@@ -0,0 +1,59 @@
+/**
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+进阶：
+
+你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ 
+
+示例 1：
+
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+示例 2：
+
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+示例 3：
+
+输入：nums = [], target = 0
+输出：[-1,-1]
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * 1.寻找第一个大于等于target的index
+ * 2.寻找第一个大于target的index
+ */
+var searchRange = function (nums, target) {
+  let ans = [-1, -1]
+  if (!nums.length) return ans
+  let leftIdx = binarySearch(nums, target, true),
+    rightIdx = binarySearch(nums, target, false) - 1
+  if (
+    leftIdx <= rightIdx &&
+    rightIdx < nums.length &&
+    nums[leftIdx] === target &&
+    nums[rightIdx] === target
+  ) {
+    ans = [leftIdx, rightIdx]
+  }
+  return ans
+}
+
+function binarySearch(nums, target, lower) {
+  let left = 0,
+    right = nums.length - 1,
+    ans = nums.length
+  while (left <= right) {
+    let mid = ((right - left) >> 1) + left
+    if (nums[mid] > target || (lower && nums[mid] >= target)) {
+      right = mid - 1
+      ans = mid
+    } else {
+      left = mid + 1
+    }
+  }
+  return ans
+}
