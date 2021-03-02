8f333f2c8e684095a7610028937017e693748a2d
Vincent.liu <1241935691@qq.com>
2021-03-02 23:40:02 +0800

    :new: ADD createMaximumNumber

--- daily/2021-03-02/createMaximumNumber.js
+++ daily/2021-03-02/createMaximumNumber.js
@@ -34,9 +34,76 @@ k = 3
  * @param {number} k
  * @return {number[]}
  */
-var maxNumber = function (nums1, nums2, k) {};
+var maxNumber = function (nums1, nums2, k) {
+  const m = nums1.length,
+    n = nums2.length
+  const maxSubsequence = new Array(k).fill(0)
+  let start = Math.max(0, k - n),
+    end = Math.min(k, m)
+  for (let i = start; i <= end; i++) {
+    const subsequence1 = findMaxSubsequence(nums1, i)
+    const subsequence2 = findMaxSubsequence(nums2, k - i)
+    const curMaxSubsequence = merge(subsequence1, subsequence2)
+    if (compare(curMaxSubsequence, 0, maxSubsequence, 0) > 0) {
+      maxSubsequence.splice(0, k, ...curMaxSubsequence)
+    }
+  }
+  return maxSubsequence
+}
 
-function findMaximumSubsequence(nums) {
-  const stack = [];
-  // for(let i=0; i<nums.length;)
+/**
+ * @description: 从nums中选取长度为k的最大子序列
+ * @param {number[]} nums
+ * @param {number} k 选取个数
+ * @return {number[]}
+ */
+function findMaxSubsequence(nums, k) {
+  let stack = [],
+    len = nums.length,
+    drop = len - k
+  for (let i = 0; i < len; i++) {
+    while (drop && stack.length && stack[stack.length - 1] < nums[i]) {
+      stack.pop()
+      drop--
+    }
+    stack.push(nums[i])
+  }
+  return stack.slice(0, k)
+}
+
+const merge = (subsequence1, subsequence2) => {
+  const x = subsequence1.length,
+    y = subsequence2.length
+  if (x === 0) {
+    return subsequence2
+  }
+  if (y === 0) {
+    return subsequence1
+  }
+  const mergeLength = x + y
+  const merged = new Array(mergeLength).fill(0)
+  let index1 = 0,
+    index2 = 0
+  for (let i = 0; i < mergeLength; i++) {
+    if (compare(subsequence1, index1, subsequence2, index2) > 0) {
+      merged[i] = subsequence1[index1++]
+    } else {
+      merged[i] = subsequence2[index2++]
+    }
+  }
+  return merged
+}
+
+const compare = (subsequence1, index1, subsequence2, index2) => {
+  const x = subsequence1.length,
+    y = subsequence2.length
+  while (index1 < x && index2 < y) {
+    const difference = subsequence1[index1] - subsequence2[index2]
+    if (difference !== 0) {
+      return difference
+    }
+    index1++
+    index2++
+  }
+  return x - index1 - (y - index2)
 }
