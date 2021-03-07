6c04404b65d6dc9f9155152ebb94a1bcb0e4c502
Vincent.liu <1241935691@qq.com>
2021-03-07 22:02:13 +0800

    :new: ADD palindrome-partitioning

---
+++ daily/2021-03-07/palindrome-partitioning.js
@@ -0,0 +1,56 @@
+/**
+ * 给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
+
+回文串 是正着读和反着读都一样的字符串。
+
+ 
+
+示例 1：
+
+输入：s = "aab"
+输出：[["a","a","b"],["aa","b"]]
+示例 2：
+
+输入：s = "a"
+输出：[["a"]]
+ 
+
+提示：
+
+1 <= s.length <= 16
+s 仅由小写英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-partitioning
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function (s) {
+  const dfs = (i) => {
+    if (i === n) {
+      ret.push(ans.slice())
+      return
+    }
+    for (let j = i; j < n; ++j) {
+      if (f[i][j]) {
+        ans.push(s.slice(i, j + 1))
+        dfs(j + 1)
+        ans.pop()
+      }
+    }
+  }
+
+  const n = s.length
+  const f = new Array(n).fill(0).map(() => new Array(n).fill(true))
+  let ret = [],
+    ans = []
+
+  for (let i = n - 1; i >= 0; --i) {
+    for (let j = i + 1; j < n; ++j) {
+      f[i][j] = s[i] === s[j] && f[i + 1][j - 1]
+    }
+  }
+  dfs(0)
+  return ret
+}
