64178b4d01f0141ddc041a1364b68971b69f87d8
hongchaoliu <hongchaoliu@6tiantian.com>
2021-03-09 17:28:40 +0800

    :pencil2: complete longest-palindromic-substring

--- daily/14-09-2020-05-longest-palindromic-substring.js
+++ daily/14-09-2020-05-longest-palindromic-substring.js
@@ -1,15 +1,33 @@
 /**
- * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 5. 最长回文子串
+给你一个字符串 s，找到 s 中最长的回文子串。
+
+ 
+
 示例 1：
 
-输入: "babad"
-输出: "bab"
-注意: "aba" 也是一个有效答案。
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
 示例 2：
 
-输入: "cbbd"
-输出: "bb"
+输入：s = "cbbd"
+输出："bb"
+示例 3：
 
+输入：s = "a"
+输出："a"
+示例 4：
+
+输入：s = "ac"
+输出："a"
+ 
+
+提示：
+
+1 <= s.length <= 1000
+s 仅由数字和英文字母（大写和/或小写）组成
+https://leetcode-cn.com/problems/longest-palindromic-substring/
 
  * @param {string} s
  * @return {string}
@@ -18,4 +36,21 @@
  * 如何划分子串：
  * 滑动窗口
  */
-var longestPalindrome = function (s) {};
+var longestPalindrome = function (s) {
+  let n = s.length,
+    f = new Array(n).fill(0).map(() => new Array(n).fill(true)),
+    left = 0,
+    right = 0;
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = i + 1; j < n; j++) {
+      f[i][j] = s[i] === s[j] && f[i + 1][j - 1];
+      if (f[i][j]) {
+        if (j - i > right - left) {
+          right = j;
+          left = i;
+        }
+      }
+    }
+  }
+  return s.substring(left, right + 1);
+};
