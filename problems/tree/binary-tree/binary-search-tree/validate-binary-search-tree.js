bd9e6cf0c712849ae6f631682e79f1ca669e4c4a
hongchaoliu <hongchaoliu@6tiantian.com>
2021-06-28 15:05:16 +0800

    :new: add 验证二叉搜索树

---
+++ problems/tree/binary-tree/binary-search-tree/validate-binary-search-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+  let stack = [],
+    cur = -Number.MAX_VALUE;
+  while (true) {
+    if (root) {
+      stack.push(root);
+      root = root.left;
+    } else if (stack.length) {
+      let n = stack.pop();
+      if (n.val <= cur) return false;
+      root = root.right;
+      cur = n.val;
+    } else {
+      break;
+    }
+  }
+  return true;
+};
