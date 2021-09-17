fb5b8cf81a8770f6504bbc9e06a1e25a56e134fb
vinvc <hongchaoliu@6tiantian.com>
2021-09-17 23:31:26 +0800

    :pencil2: change syntax

--- problems/array/dp/零钱兑换.js
+++ problems/array/dp/零钱兑换.js
@@ -92,7 +92,7 @@ var coinChangeDp = function (coins, amount) {
   for (let i = 1; i <= amount; i++) {
     for (let j = 0; j < coins.length; j++) {
       if (coins[j] <= i) {
-        dp[i] = Math.min(dp[i], dp(i - coins[j]) + 1);
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
       }
     }
   }
