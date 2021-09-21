84aa54311b8b5e3b0d1b33157e1076e00dad57b0
vinvc <hongchaoliu@6tiantian.com>
2021-09-21 18:34:29 +0800

    :pencil2: complete lowestCommonAncestor

--- problems/tree/binary-tree/binary-search-tree/最近公共祖先.js
+++ problems/tree/binary-tree/binary-search-tree/最近公共祖先.js
@@ -1,4 +1,4 @@
-/**
+/**https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
@@ -14,4 +14,18 @@
  * 寻找最近公共祖先的思路：
  *
  */
-var lowestCommonAncestor = function (root, p, q) {};
+var lowestCommonAncestor = function (root, p, q) {
+  if (!root) return null
+  if (root === p || root === q) return root
+  let left = lowestCommonAncestor(root.left, p, q)
+  let right = lowestCommonAncestor(root.right, p, q)
+  if (left && right) {
+    return root
+  } else if (left) {
+    return left
+  } else if (right) {
+    return right
+  } else {
+    return null
+  }
+}
